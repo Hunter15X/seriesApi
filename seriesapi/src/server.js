f938ac5374cc5c78918917c3f8af0fa3c2f538c1
Hunter15X <gusvarjao15@gmail.com>
2020-02-07 14:55:14 -0300

    Segundo commit

--- seriesapi/src/server.js
+++ seriesapi/src/server.js
@@ -4,7 +4,15 @@ const auth = require('./routes/authRoutes')
 
 const app = express()
 
+let allowCrossDomain = (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Header', '*')
+    next()
+}
+
 app.use(express.json())
+app.use(allowCrossDomain)
+
 app.use('/auth', auth)
 app.use('/series', seriesRoutes)
 
--- seriesweb/src/App.js
+++ seriesweb/src/App.js
@@ -1,13 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      React js
-    </div>
-  );
+class App extends Component{
+
+  constructor(){
+    super()
+    this.state = {
+      lista:[]
+    }
+  }
+
+  async componentDidMount(){
+    const resposta = await fetch('http://localhost:3000/series');
+    const series = await resposta.json()
+    
+    console.log(series)
+
+    this.setState({
+      lista: series
+    })
+
+  }
+
+  render(){
+    console.log('render()')
+    return (
+      <div className="App">
+        Cadastro de Séries
+          <form method="post">
+            <div className="form">
+              <label htmlFor="nome">Nome</label>
+              <input type="text" id="nome" name="nome" />
+              <label htmlFor="nome">Ano de Lançamento</label>
+              <input type="text" id="ano_lanc" name="ano_lanc" />
+              <label htmlFor="nome">Temporadas</label>
+              <input type="text" id="temporada" name="temporada" />
+              <label htmlFor="sinopse">Sinopse</label>
+              <textarea id="sinopse" name="sinopse"></textarea>
+
+              <button type="submit">Salvar</button>
+            </div>
+          </form>
+
+          <div className="list">
+              <table border="1" width="800px" height="200px">
+                <thead>
+                    <tr>
+                          <th width="200px">
+                            Nome
+                          </th>
+                          <th width="200px">
+                            Ano de Lançamento
+                          </th>
+                          <th width="200px">
+                            Temporadas
+                          </th>
+                          <th width="200px">
+                            Sinopse
+                          </th>
+                    </tr>
+                 </thead> 
+              </table>
+
+              <table  width="800px" height="200px">
+                  <thead>
+                      {this.state.lista.map(serie => {
+                        return (
+                            <tr key={serie.id}>
+                            <th width="200px">
+                              {serie.nome}
+                            </th>
+                            <th width="200px">
+                              {serie.ano_de_lancamento}
+                            </th>
+                            <th width="200px">
+                            {serie.temporadas}
+                            </th>
+                            <th width="200px">
+                            {serie.sinopse}
+                            </th>
+                          </tr>
+                        )
+                      })}
+                  </thead> 
+              </table>
+
+          </div>
+      </div>
+    );
+  }
 }
 
 export default App;
